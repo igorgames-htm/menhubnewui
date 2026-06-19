487a501cecab00c48e0f3a3d7af3ab2ff1ba8ae9
igorgames-htm <sigmasigma14653@gmail.com>
2026-06-19 22:50:52 +0200

    Update Library.lua

--- Dependencies/Library.lua
+++ Dependencies/Library.lua
@@ -2681,7 +2681,7 @@ function Library:CreateWindow(...)
 
     local WinW, WinH
     do
-        WinW = IsTouch and 580 or 380; WinH = IsTouch and 400 or 680
+        WinW = IsTouch and 380 or 780; WinH = IsTouch and 400 or 680
         if Config.Center then
             Config.AnchorPoint = Vector2.zero
             Config.Position    = UDim2.new(0.5, -math.floor(WinW / 2), 0.5, -math.floor(WinH / 2))
